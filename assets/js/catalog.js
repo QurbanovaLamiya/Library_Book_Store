5e5ab3a14ae2512d6c2bbf1a87f6044aad1874b6
mammadli02 <memmedliyazgul2002@gmail.com>
2022-08-09 16:06:57 +0400

    created catalog page

---
+++ assets/js/catalog.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $(".owl-carousel1").owlCarousel({
+      autoplay: true,
+      autoplayTimeout: 3000,
+      loop: true,
+      margin: 30,
+      nav: true,
+      navText: ["<img src='./assets/images/prev.svg'>", "<img src='./assets/images/next.svg'>"],
+      responsive: {
+        0: {
+          items: 1,
+        },
+        577: {
+          items: 2,
+        },
+        1100: {
+          items: 4,
+        },
+        1200: {
+          items: 5,
+        },
+      },
+    });
+})
