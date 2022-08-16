d54b3c9382a821cd496039c316fdc4351bcf9c8d
QurbanovaLamiya <lamiyaqurbanova121@gmail.com>
2022-08-16 14:17:17 +0400

    Update pages design

--- assets/js/admin/admin.js
+++ assets/js/admin/admin.js
@@ -1,12 +1,29 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  let adminLoginForm = $("#adminLoginForm");
+  let adminPanel = $("#AdminPanel");
+  let adminJoinButton = $("#adminJoinButton");
 
-  let sidebar = $('.sidebar')
-    $('.hamburger').click(function () {
-      $('#adminpanel').hide()          
-       sidebar.show()
-    })
-    $('.x-image').click(function() {
-      sidebar.hide()      
-      $('#adminpanel').show()
-    })
-})
+  adminJoinButton.on("click", function (e) {
+    e.preventDefault();
+    adminLoginForm.hide();
+    adminPanel.removeCalss
+    adminPanel.show();
+  });
+
+  // if(adminLoginForm) {
+  //   adminLoginForm.show()
+  //   adminPanel.hide()
+  // }else{
+  //   adminLoginForm.hide()
+  //   adminPanel.show()
+  // }
+  // let sidebar = $('.sidebar')
+  //   $('.hamburger').click(function () {
+  //     $('#adminpanel').hide()
+  //      sidebar.show()
+  //   })
+  //   $('.x-image').click(function() {
+  //     sidebar.hide()
+  //     $('#adminpanel').show()
+  //   })
+});
