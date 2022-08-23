b77e2c75a200f1f41fad1aef35df1273a373a542
mammadli02 <memmedliyazgul2002@gmail.com>
2022-08-23 10:01:35 +0400

    created booksearch

--- assets/js/admin/admin.js
+++ assets/js/admin/admin.js
@@ -10,3 +10,182 @@ $(document).ready(function() {
       $('#adminpanel').show()
     })
 })
+$(document).ready(function () {
+  let db = firebase.database();
+  let bookDesc = $("#bookDesc");
+  let addNewBook = db.ref("/addNewBook");
+  let addAboutNewStore = db.ref("/addAboutNewStore");
+
+
+
+  
+ 
+  const bookForm = document.querySelector("#bookForm");
+  let searchAdminInput = $("#AdminSearch");
+  let searchAdminResult = $("#AdminSearchResult");
+  let searchResultData = null;
+  let searchChooseBookData = null;
+  let searchBookFormInputField = null;
+
+  //Catching Book details
+  let bookName = $("#bookName");
+  let authorName = $("#authorName");
+  let bookImageUrl = $("#bookImageUrl");
+  let year = $("#publicationYear");
+  let isNew = $("#isNew");
+  let bookSelectType = $("#bookSelectType");
+
+  //Search starts at least after 4 characters enterred
+  searchAdminInput.on("input", function () {
+    let value = $(this).val();
+    if (value.length < 4) {
+      searchAdminResult.addClass("d-none");
+      return;
+    }
+
+    if (value.trim() === "") {
+      return;
+    }
+
+    searchAdminResult.removeClass("d-none");
+    getBook(value);
+  });
+  
+  bookDesc.on("input", function () {
+    let descArea = $(this).val(searchChooseBookData.volumeInfo.authors);
+    $(this).siblings("span").html(` ${descArea.length} / 100`);
+    $(this).siblings("span").removeClass("text-danger");
+
+    if (descArea.length > 100) {
+      textAreaCount = true;
+      $(this).siblings("span").addClass("text-danger");
+    }
+  });
+  //Add book type selection
+  
+
+  //Search result drop down box pops up after 4 charachters enterred
+  function searchResultDropdownRender(data) {
+    let searchResultList = $(".search-result-list");
+
+    if (data.error) {
+      searchResultList.html(
+        `<p class="text-danger mt-5 text-center h3">${data.message}</p>`
+      );
+
+      setTimeout(() => {
+        searchAdminResult.addClass("d-none");
+      }, 2000);
+      return;
+    }
+    $(document).on("click", ".search-result-item", function () {
+      let selectBookID = $(this).attr("id");
+      console.log(selectBookID);
+      searchChooseBookData = searchResultData.find(
+        (book) => book.id === selectBookID
+      );
+      console.log("searchChooseBookData", searchChooseBookData);
+      searchAdminResult.addClass("d-none");
+      //bookname
+      let bookData = searchChooseBookData.volumeInfo;
+      bookName.val(bookData.title);
+      //author name
+      authorName.val(bookData.authors);
+
+      //book publish year
+      if (bookData.publishedDate) {
+        let publishYear = parseInt(bookData.publishedDate.substring(0, 4));
+        let thisYear = new Date().getFullYear();
+        year.val(publishYear);
+      }
+
+      //description
+      bookDesc.val(bookData.description);
+
+      //isNew
+      isNew.prop("checked", true);
+
+      //Book type
+      bookSelectType.val(searchChooseBookData.volumeInfo.printType);
+      searchAdminInput.val("");
+
+    });
+
+    searchResultList.html(
+      data
+        .map(
+          (book) => `
+        <div class="search-result-item cursor-pointer" id=${book.id}>
+          <p class="lead">
+              <img src="./assets/images/clock.svg" witdh="50" class="mr-2" alt="">
+              <span>
+              ${
+                book.volumeInfo.authors?.join(", ") || ""
+              } ${book.volumeInfo.title.slice(0, 23)}...
+              </span>
+          </p>
+        </div>
+  `
+        )
+        .join("")
+    );
+  }
+
+    //Write about store data to firebase
+    $("#aboutStoreFormSubmit").on("click", function (event) {
+      event.preventDefault();
+  
+      let aboutStoreTitle = $("#aboutStoreTitle").val();
+      let aboutStoreDesc = $("#aboutStoreDesc").val();
+      let aboutStoreImageUrl = $("aboutStoreImageUrl").val();
+  
+      addAboutNewStore.push().set({
+        aboutStoreTitle,
+        aboutStoreDesc,
+        aboutStoreImageUrl
+      });
+    });
+
+  //Write bookdata to firebase
+  $("#bookFormSubmit").on("click", function (event) {
+    event.preventDefault();
+ 
+    let bookName = $("#bookName").val().trim();
+    let authorName = $("#authorName").val().trim();
+    let bookImageUrl = $("#bookImageUrl").val().trim();
+    let year = $("#publicationYear").val().trim();
+    let isNew = $("#isNew");
+    let bookDesc = $("#bookDesc").val().trim();
+
+    addNewBook.push().set({
+      bookName,
+      authorName,
+      bookImageUrl,
+      year,
+      isNew,
+      bookDesc,
+      bookSelectType,
+    });
+    bookForm.reset();
+  });
+
+
+  // Get books from endpoint
+
+  async function getBook(bookName) {
+    let url = `https://www.googleapis.com/books/v1/volumes?q=${bookName}`;
+    const body = {
+      method: "GET",
+    };
+    try {
+      let { items } = await $.ajax(url, body);
+      searchResultData = items;
+      console.log(searchResultData);
+      searchResultDropdownRender(searchResultData);
+    } catch (err) {
+      alert('Server Error')
+      console.log(err);
+    }
+  }
+
+})
---
+++ assets/js/db.js
@@ -0,0 +1,35 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDwGNH3RxILSlbrtL145NAdUZUNKBIbvFI",
+    authDomain: "library-store-cb5fb.firebaseapp.com",
+    databaseURL: "https://library-store-cb5fb-default-rtdb.firebaseio.com",
+    projectId: "library-store-cb5fb",
+    storageBucket: "library-store-cb5fb.appspot.com",
+    messagingSenderId: "209356641156",
+    appId: "1:209356641156:web:6bdc60c65318799d84c3d1",
+  };
+  
+
+  firebase.initializeApp(firebaseConfig);
+  let db= firebase.database();
+
+
+
+
+
+
+
+
+
+//   const firebaseConfig = {
+//     apiKey: "AIzaSyBlUlK8J6pwEobsFdZhVRHGpcTLrO4_-ME",
+//     authDomain: "library-project-b73e8.firebaseapp.com",
+//     databaseURL: "https://library-project-b73e8-default-rtdb.firebaseio.com",
+//     projectId: "library-project-b73e8",
+//     storageBucket: "library-project-b73e8.appspot.com",
+//     messagingSenderId: "63658460406",
+//     appId: "1:63658460406:web:fd8882e205cb47797558d7"
+//   };
+
+//   firebase.initializeApp(firebaseConfig)
+
+//   let db = firebase.database();
